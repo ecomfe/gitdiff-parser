bdebfbac390406ddc92d0f3fc9aad31850a85eb0
huangfengli <huangfengli@baidu.com>
2020-03-24 10:58:37 +0800

    FIX: declaration

--- index.d.ts
+++ index.d.ts
@@ -1,38 +1,38 @@
-export interface Change {
-    content: string;
-    type: 'insert' | 'delete' | 'normal';
-    isInsert?: boolean;
-    isDelete?: boolean;
-    isNormal?: boolean;
-    lineNumber?: number;
-    oldLineNumber?: number;
-    newLineNumber?: number;
-}
+declare module 'gitdiff-parser' {
+    export interface Change {
+        content: string;
+        type: 'insert' | 'delete' | 'normal';
+        isInsert?: boolean;
+        isDelete?: boolean;
+        isNormal?: boolean;
+        lineNumber?: number;
+        oldLineNumber?: number;
+        newLineNumber?: number;
+    }
 
-export interface Hunk {
-    content: string;
-    oldStart: number;
-    newStart: number;
-    oldLines: number;
-    newLines: number;
-    changes: Change[];
-}
+    export interface Hunk {
+        content: string;
+        oldStart: number;
+        newStart: number;
+        oldLines: number;
+        newLines: number;
+        changes: Change[];
+    }
 
-export interface File {
-    hunks: Hunk[];
-    oldEndingNewLine: boolean;
-    newEndingNewLine: boolean;
-    oldMode: string;
-    newMode: string;
-    similarity?: number;
-    oldRevision: string;
-    newRevision: string;
-    oldPath: string;
-    newPath: string;
-    isBinary?: boolean;
-    type: 'add' | 'delete' | 'modify' | 'rename' | 'copy';
-}
+    export interface File {
+        hunks: Hunk[];
+        oldEndingNewLine: boolean;
+        newEndingNewLine: boolean;
+        oldMode: string;
+        newMode: string;
+        similarity?: number;
+        oldRevision: string;
+        newRevision: string;
+        oldPath: string;
+        newPath: string;
+        isBinary?: boolean;
+        type: 'add' | 'delete' | 'modify' | 'rename' | 'copy';
+    }
 
-export default {
-    parse(source: string): File[];
-};
+    export function parse(source: string): File[];
+}
