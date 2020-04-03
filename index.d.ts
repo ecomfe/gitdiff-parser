100c5281e5c3ae96ba4c37c2bd295b422baa36c2
huangfengli <huangfengli@baidu.com>
2020-04-03 10:33:08 +0800

    declare module 是用在第三方库的声明

--- index.d.ts
+++ index.d.ts
@@ -1,38 +1,42 @@
-declare module 'gitdiff-parser' {
-    export interface Change {
-        content: string;
-        type: 'insert' | 'delete' | 'normal';
-        isInsert?: boolean;
-        isDelete?: boolean;
-        isNormal?: boolean;
-        lineNumber?: number;
-        oldLineNumber?: number;
-        newLineNumber?: number;
-    }
-
-    export interface Hunk {
-        content: string;
-        oldStart: number;
-        newStart: number;
-        oldLines: number;
-        newLines: number;
-        changes: Change[];
-    }
-
-    export interface File {
-        hunks: Hunk[];
-        oldEndingNewLine: boolean;
-        newEndingNewLine: boolean;
-        oldMode: string;
-        newMode: string;
-        similarity?: number;
-        oldRevision: string;
-        newRevision: string;
-        oldPath: string;
-        newPath: string;
-        isBinary?: boolean;
-        type: 'add' | 'delete' | 'modify' | 'rename' | 'copy';
-    }
-
-    export function parse(source: string): File[];
+export interface Change {
+    content: string;
+    type: 'insert' | 'delete' | 'normal';
+    isInsert?: boolean;
+    isDelete?: boolean;
+    isNormal?: boolean;
+    lineNumber?: number;
+    oldLineNumber?: number;
+    newLineNumber?: number;
 }
+
+export interface Hunk {
+    content: string;
+    oldStart: number;
+    newStart: number;
+    oldLines: number;
+    newLines: number;
+    changes: Change[];
+}
+
+export interface File {
+    hunks: Hunk[];
+    oldEndingNewLine: boolean;
+    newEndingNewLine: boolean;
+    oldMode: string;
+    newMode: string;
+    similarity?: number;
+    oldRevision: string;
+    newRevision: string;
+    oldPath: string;
+    newPath: string;
+    isBinary?: boolean;
+    type: 'add' | 'delete' | 'modify' | 'rename' | 'copy';
+}
+
+declare type GitdiffParser = {
+    parse(source: string): File[];
+}
+
+declare const gitdiffParser: GitdiffParser;
+
+export default gitdiffParser;
